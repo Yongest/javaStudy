5dd827d6e0d58b77e30aac251468c1860e70a3f4
yongest <1046788379@qq.com>
2019-03-13 15:41:48 +0800

    数组动态初始化

---
+++ myArray/src/com/itheima/ArrayDemo.java
@@ -0,0 +1,16 @@
+package com.itheima;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        int[] arr  = new int[3];
+
+        /*
+        * int : 说明数组中的元素是int类型，
+        * [] 说明这是一个数组
+        * arr:这是数组的名称
+        *
+        * new 为数组申请内存空间
+        * 3 数组的长度
+        * */
+    }
+}
